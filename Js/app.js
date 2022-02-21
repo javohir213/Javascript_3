ec5d979dec76f3fab2d354c512f8b2cdf22088d8
javohir213 <ajavohir213@gmail.com>
2022-02-21 23:18:38 +0500

    Initial commit

---
+++ Js/app.js
@@ -0,0 +1,25 @@
+// var name="Javohir";
+// var age="20";
+
+
+// var person={
+//  name:"Javohir",
+//  age:20 
+// }
+// console.log(person);
+
+var mony = prompt("summani kiriting");
+var a = 500;
+var b = 250;
+var c = 120;
+var x = (a * 10900);
+var y = (b * 10900);
+var z = (c * 12273);
+var result=(x + y + z);
+
+if (mony >= result) {
+    console.log("Oq yo'l, Alisher!");
+}
+else{
+    console.log(`Alisher, ozgina sabr qilish kerak bo’lar ekan.`);
+}
